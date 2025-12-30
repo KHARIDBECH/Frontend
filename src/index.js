34d31baae135b0b31e4420ddab35edbe1a400048
Mohsin0786 <mk67205@gmail.com>
2025-12-30 13:16:18 +0530

    vercel anayltics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 
 import { AuthContextProvider } from './AuthContext';
 
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <App />
+      <Analytics />
     </AuthContextProvider>
   </React.StrictMode>
 );
